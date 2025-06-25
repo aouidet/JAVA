d12a92d3bbede8a0736935d25edd7ec44ff00d92
aouidet oussama <aouidetoussama@mbp-de-aouidet.home>
2025-06-25 14:47:18 +0200

    init project

---
+++ src/main/java/com/practices/practice/feature/java/PracticeFeatureJavaApplication.java
@@ -0,0 +1,13 @@
+package com.practices.practice.feature.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeFeatureJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeFeatureJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practices/practice/feature/java/PracticeFeatureJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practices.practice.feature.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeFeatureJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
